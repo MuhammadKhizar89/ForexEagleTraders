e42478db7b0bf636dda5c189e9c43943ce5f2a1f
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-05-07 20:21:25 +0500

    userloadingbar1

--- Client/src/Components/Work.jsx
+++ Client/src/Components/Work.jsx
@@ -2,6 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
+// Modal component for displaying loading indicator
+const LoadingModal = () => {
+  return (
+    <div className="fixed top-0 left-0 z-50 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
+      <div className="bg-white p-8 rounded-lg">
+        <div className="flex items-center mb-4">
+          <div className="rounded-full h-4 w-4 bg-gray-400 mr-4"></div>
+          <div className="text-gray-800">Loading...</div>
+        </div>
+        <div className="bg-gray-300 h-2 rounded-full">
+          <div className="bg-blue-500 h-full rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function Work() {
   const [attendanceData, setAttendanceData] = useState([]);
   const [userData, setUserData] = useState({});
@@ -48,6 +65,7 @@ export default function Work() {
 
   return (
     <div className='w-full h-screen bg-gradient-to-b from-black to-gray-800'>
+      {isLoading && <LoadingModal />} {/* Render LoadingModal if isLoading is true */}
       <div className="container mx-auto p-4 bg-gradient-to-b from-black to-gray-800 h-screen">
         <div className="mb-8 ">
           <h2 className="text-2xl font-bold mb-2 text-white">User Info</h2>
@@ -60,9 +78,7 @@ export default function Work() {
 
         <div>
           <h2 className="text-2xl font-bold mb-2 text-white">Attendance</h2>
-          {isLoading ? (
-            <p>Loading...</p> // Render loading indicator while data is being fetched
-          ) : (
+          {!isLoading && (
             <div className="bg-white shadow-md rounded-lg p-4">
               <table className="table-auto w-full">
                 <thead>
