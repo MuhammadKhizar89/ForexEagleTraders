dc68325504e483e01ba8e1ce9105eb8ada8b1cd0
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-05-07 13:23:34 +0500

    validaion added1

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -89,7 +89,6 @@ export default function Login() {
                 className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${loginErrors.email && 'border-red-500'}`}
                 placeholder="Your Email"
               />
-              {loginErrors.email && <p className="text-red-500 text-xs italic">{loginErrors.email}</p>}
             </div>
             <div className="mb-4">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
@@ -102,8 +101,8 @@ export default function Login() {
                 className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${loginErrors.password && 'border-red-500'}`}
                 placeholder="Your Password"
               />
-              {loginErrors.password && <p className="text-red-500 text-xs italic">{loginErrors.password}</p>}
             </div>
+            {loginErrors.email && <p className="text-red-500 text-xs italic">{loginErrors.email}</p>}
             <div className="flex justify-center">
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                 Login
